c1ab8de0af59ca5d1384f284402a3f023ca5cda1
hussamss <hussams@hotmail.ca>
2017-05-17 09:36:57 +0300

    new chnage for working

--- js/loadIndex.js
+++ js/loadIndex.js
@@ -18,41 +18,16 @@
 
 // });
 
-// function getLocation(callback) {
-//     var promise = new Promise(function(resolve, reject) {
-//         if (navigator.geolocation) {
-//             navigator.geolocation.getCurrentPosition(
-//                 function(position){
-//                     resolve("@" + position.coords.latitude + "," + position.coords.longitude)
-//                 }
-//             );
-//         } else {
-//           reject("Unknown");
-//         }
-//     });
-
-//     return promise;
-// }
-
-// var locationPromise = getLocation();
-// locationPromise
-//       .then(function(loc) { console.log(loc); })
-//       .catch(function(err) { console.log("No location"); });
-
 function getLocation(callback) {
     var promise = new Promise(function(resolve, reject) {
         if (navigator.geolocation) {
-        	
             navigator.geolocation.getCurrentPosition(
                 function(position){
                     resolve( position.coords.latitude + "," + position.coords.longitude)
                 }
             );
         } else {
-        	
- 			window.location.href = "weather.html"; //For browser  that had no geolocation objects//
-          // reject("Unknown");
-
+          reject("Unknown");
         }
     });
 
@@ -60,15 +35,74 @@ function getLocation(callback) {
 }
 
 var locationPromise = getLocation();
-console.log(locationPromise)
-locationPromise.then(function(loc) { 
-	window.location.href = "weather.html"; 
-	// This for SPA, single page application
+// locationPromise
+//       .then(function(loc) { console.log(loc); })
+//       .catch(function(err) { console.log("No location"); });
 
-	// 	var url = "weather.html"
-	// $('#wholePage').load(url + ' #wholePage').hide().delay( 3000 ).fadeIn( 3000 );
+// function getLocation(callback) {
+//     var promise = new Promise(function(resolve, reject) {
+//         if (navigator.geolocation) {
+        	
+//             navigator.geolocation.getCurrentPosition(
+//                 function(position){
+//                     resolve( position.coords.latitude + "," + position.coords.longitude)
+//                 }
+//             );
+//         } else {
+        	
+//  			window.location.href = "weather.html"; //For browser  that had no geolocation objects//
+//           // reject("Unknown");
 
- })
+//         }
+//     });
+
+//     return promise;
+// }
+
+// var locationPromise = getLocation();
+// console.log(locationPromise)
+// locationPromise.then(function(loc) { 
+// 	window.location.href = "weather.html"; 
+// 	// This for SPA, single page application
+
+// 	// 	var url = "weather.html"
+// 	// $('#wholePage').load(url + ' #wholePage').hide().delay( 3000 ).fadeIn( 3000 );
+
+//  })
+
+
+
+
+navigator.permissions.query({name:'geolocation'})
+  .then(function(permissionStatus) {  
+    console.log('geolocation permission state is ', permissionStatus.state);
+    if(permissionStatus.state==="denied") {
+  			setTimeout(function () {
+    	   window.location.href = "weather.html"; //will redirect to your blog page (an ex: weather.html)
+    	    }, 1500); //will call the function after 2 secs.
+  	}else if(permissionStatus.state==="granted") {
+  		   setTimeout(function () {
+    	   window.location.href = "weather.html"; //will redirect to your blog page (an ex: weather.html)
+    	    }, 1500); //will call the function after 2 secs.
+  	}
+    
+
+    permissionStatus.onchange = function(result) {
+
+      console.log('geolocation permission state has changed to ', this.state);
+      if(this.state==="denied"){
+      	window.location.href = "weather.html"; 
+  		} else if(this.state==="granted") {
+  		   setTimeout(function () {
+    	   window.location.href = "weather.html"; //will redirect to your blog page (an ex: weather.html)
+    	    }, 3000); //will call the function after 2 secs.
+  		}
+
+      //Here in the next few line is to try to make the page load dynamically but 
+   	   // 		var url = "weather.html"
+			    // $('#wholePage').load(url + ' #wholePage').hide().delay( 3000 ).fadeIn( 3000 );
+    };
+  });
 		
 
 
--- js/locationDetection.js
+++ js/locationDetection.js
@@ -5,14 +5,14 @@ navigator.geolocation.getCurrentPosition(success, error);
     function success(position) {
 
 // 
-    var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
+    // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
     // Those geocoding to test other city across the country
     // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 49.2827 + '%2C' + -123.116226 + '&language=en';//Vancouver test
     // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 51.131470 + '%2C' + -114.010559 + '&language=en';//Calgary test
     // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 52.146973 + '%2C' + -106.647034 + '&language=en';//Saskatoon test
-    // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 49.895077 + '%2C' + -97.138451+ '&language=en';//Winnpeg test
-    var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 43.653908 + '%2C' + -79.384293+ '&language=en';//Toronto test
-    
+    var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 49.895077 + '%2C' + -97.138451+ '&language=en';//Winnpeg test
+    // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 43.653908 + '%2C' + -79.384293+ '&language=en';//Toronto test
+    console.log("I am running in the here")
     
 
     // get the location data from google maps api 
@@ -20,7 +20,7 @@ navigator.geolocation.getCurrentPosition(success, error);
         // fetch the results to find the City and Province for auto detect of weather function
         // location.results[3] contains the address of the city, province, country
         var tempLocation = location.results[3].formatted_address;
-       
+
         // regExp for the city and province 
         var addressPat = /(\w+|\w+ \w+), (AB|BC|ON|MB|QC|SK|NL|NS|NU|NT|YT|PE|NB)/;
         var cityProName = addressPat.exec(tempLocation);
